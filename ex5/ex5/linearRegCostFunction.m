4a7ca56e04df396020ba16918ffa30344e74cbec
Sameer Adhikari <sameer.adhikari@oracle.com>
2017-05-16 23:26:16 -0700

    completed regularized linear regression cont function and gradient

--- ex5/ex5/linearRegCostFunction.m
+++ ex5/ex5/linearRegCostFunction.m
@@ -18,17 +18,9 @@ grad = zeros(size(theta));
 %
 %               You should set J to the cost and grad to the gradient.
 %
-
-
-
-
-
-
-
-
-
-
-
+J = (sum((X * theta - y) .^ 2) + (sum(theta(2:end) .^ 2) * lambda)) / (2 * m);
+grad = (sum((X * theta - y) .* X)) / m;
+grad(2:end) = grad(2:end) .+ ((lambda/ m ) * theta'(2:end));
 
 % =========================================================================
 
