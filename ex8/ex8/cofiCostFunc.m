90fbcc86b0c5abf0956219168b8f99b80d6d5172
Sameer Adhikari <sameer.adhikari@oracle.com>
2017-06-03 17:06:08 -0700

    completed part 4 of 6

--- ex8/ex8/cofiCostFunc.m
+++ ex8/ex8/cofiCostFunc.m
@@ -40,9 +40,12 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 predictions = X * Theta'; % this is written differently than how it is on paper where is it Theta' * x(i)
-errors = predictions - Y;
-reduction = errors .* R; % Use an error only of R(i, j) == 1
-J = sum(sum(reduction .^ 2) / 2);
+errors = predictions - Y; % num_movies x num_users
+errors_filtered = errors .* R; % Use an error only of R(i, j) == 1
+J = sum(sum(errors_filtered .^ 2) / 2);
+
+X_grad = errors_filtered * Theta;
+Theta_grad = errors_filtered' * X;
 
 % =============================================================
 
